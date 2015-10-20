ea6f9a71a7157c21691e40d2710d3978b31d41d0
Dmitry <dima-legkov@mail.ru>
2015-10-20 21:48:08 +0300

    1.3.2

---
+++ laba2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double one, tho;
+    setlocale(LC_ALL,"Russian");
+    cout << "¬ведите первое число :";
+    cin >> one;
+    cout << "¬ведите второе число :";
+    cin >> tho;
+}
